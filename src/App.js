19f6eab2005a602548245eaafc4752974a613f5f
Raphael Coelho de Lima <rcl.lp@hotmail.com>
2022-10-18 17:28:13 -0400

    NetflixClone01

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import Tmbd from './Tmbd';
+
 
 export default () => {
+
+  useEffect(() =>{
+    const loadAll = async () => {
+      //pegando a lista total
+      let list = await Tmbd.getHomeList()
+      console.log(list)
+    }
+
+    loadAll()
+  },[])
+
   return (
-    <div>Ola Mundo!!</div>
+    <div>Ola Mundo!</div>
   );
 }
---
+++ src/Tmbd.js
@@ -0,0 +1,67 @@
+const API_KEY = '53a3315307f6f168c63cc26af477fb9f'
+const API_BASE = 'https://api.themoviedb.org/3'
+
+/*
+- originais
+- recomendados (treding)
+- em alta (top rated)
+- acao
+- comedia
+- terror
+- romance
+- documentarios
+*/
+
+ const basicFetch = async(endpoint) => {
+    const req = await fetch(`${API_BASE}${endpoint}`)
+    const json = await req.json()
+    return json
+ }
+
+
+export default {
+    getHomeList: async () => {
+        return [
+            {
+                slug: 'originais',
+                title: 'Originais do Netflix',
+                items: await basicFetch(`/discover/tv?with_network=213&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'treding',
+                title: 'Recomendados para Você',
+                items: await basicFetch(`/trending/all/week?api_key=${API_KEY}`)
+            },
+            {
+                slug: 'toprated',
+                title: 'Em Alta',
+                items: await basicFetch(`/movie/top_rated?api_key=${API_KEY}`)
+            },
+            {
+                slug: 'action',
+                title: 'Ação',
+                items: await basicFetch (`/discover/movie?with_genres=28&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'comedy',
+                title: 'Comédia',
+                items: await basicFetch (`/discover/movie?with_genres=35&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'horror',
+                title: 'Terror',
+                items: await basicFetch (`/discover/movie?with_genres=27&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'romance',
+                title: 'Romance',
+                items: await basicFetch (`/discover/movie?with_genres=10749&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'documentary',
+                title: 'Documentários',
+                items: await basicFetch (`/discover/movie?with_genres=99&api_key=${API_KEY}`)
+            },
+        ]
+    }
+}
