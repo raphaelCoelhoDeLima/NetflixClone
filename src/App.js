d35c4cb8fdc36f45cb8f48af4e162a2ffd38c6b1
Raphael Coelho de Lima <rcl.lp@hotmail.com>
2022-10-23 17:20:01 -0400

    ListasNetflixClone

--- src/App.js
+++ src/App.js
@@ -1,20 +1,39 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Tmbd from './Tmbd';
+import MovieRow from './components/MovieRow';
+import './App.css'
+import FeaturedMovie from './components/FeaturedMovie';
 
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default () => {
+
+  const [movieList, setMovieList] = useState([])
+  const [featuredData, setFeaturedData] = useState(null)
 
   useEffect(() =>{
     const loadAll = async () => {
       //pegando a lista total
       let list = await Tmbd.getHomeList()
-      console.log(list)
+      setMovieList(list)
     }
 
     loadAll()
   },[])
 
   return (
-    <div>Teste</div>
+    <div className='page'>
+     
+     {featuredData &&
+     <FeaturedMovie />
+     }
+     
+     <section className='lists'>
+      {movieList.map((item, key)=>(
+      <MovieRow key={key} title={item.title} items={item.items}/>
+      ))}
+     </section>
+
+    </div>
   );
 }
---
+++ src/components/FeaturedMovie.js
@@ -0,0 +1,11 @@
+/* eslint-disable import/no-anonymous-default-export */
+import React from "react";
+import './FeaturedMovie.css'
+
+export default () => {
+    return (
+        <div>
+            Filme em Destaque
+        </div>
+    )
+}
