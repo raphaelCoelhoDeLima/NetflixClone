cbd8952cebda0180d9d633c7010373ef8f2684bb
Raphael Coelho de Lima <rcl.lp@hotmail.com>
2022-10-23 21:36:33 -0400

    header add

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Tmbd from './Tmbd';
 import MovieRow from './components/MovieRow';
 import './App.css'
 import FeaturedMovie from './components/FeaturedMovie';
+import Header from './components/Header';
 
 
 // eslint-disable-next-line import/no-anonymous-default-export
@@ -10,6 +11,7 @@ export default () => {
 
   const [movieList, setMovieList] = useState([])
   const [featuredData, setFeaturedData] = useState(null)
+  const [blackHeader, setBlackHeader] = useState(false)
 
   useEffect(() =>{
     const loadAll = async () => {
@@ -28,8 +30,24 @@ export default () => {
     loadAll()
   },[])
 
+  useEffect(()=>{
+    const scrollListener = () => {
+      if(window.scrollY >10){
+        setBlackHeader(true)
+      } else {
+        setBlackHeader(false)
+      }
+    }
+    window.addEventListener('scroll', scrollListener)
+
+    return ()=> {
+      window.removeEventListener('scroll', scrollListener)
+    }
+  },[])
+
   return (
     <div className='page'>
+      <Header black={blackHeader} />
      
      {featuredData &&
      <FeaturedMovie item={featuredData}/>
--- src/components/FeaturedMovie.js
+++ src/components/FeaturedMovie.js
@@ -18,21 +18,20 @@ export default ({item}) => {
         }}>
             <div className="featured--vertical">
                 <div className="featured--horizontal">
-                    <div className="featured--name">{item.original_name}
+                    <div className="featured--name">{item.original_name}</div>
                     <div className="featured--info">
                         <div className="featured--points">{item.vote_average} Pontos</div>
                         <div className="featured--year" >{firstDate.getFullYear()}</div>
                         <div className="featured--season">{item.number_of_seasons} Temporada{item.number_of_seasons !== 1 ? 's' : ''}</div>
                         <div className="featured--description">{item.overview}</div>
                         <div className="featured--buttons" ></div>
+                        
+                            <a href={`/watch/${item.id}`} className="featured--watchbutton">▶ Assistir</a>
+                            <a href={`/list/add${item.id}`} className="featured--mylistbutton">+ Minha Lista</a>
+                        
+                    </div>
                         <div className="featured--genres" ><strong>Gêneros: </strong>{genres.join(', ')}</div>
-
-                    </div>
-
-                    </div>
-
                 </div>
-                
             </div>
 
         </section>
